f36835cc57e5ce6bc58ac74abe2a4393ca4dc954
Vamshedhar Reddy <vamshedhar@gmail.com>
2015-02-05 01:02:13 +0530

    feat(): wrote base admin abstract class.

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class PhoneBookBaseAdmin(admin.ModelAdmin):
+
+  class Meta:
+    abstract = True
+
+  def get_queryset(self, request):
+
+    # Override queryset to show only objects which are not deleted.
+    query = super(PhoneBookBaseAdmin, self).get_queryset(request)
+    if request.user.is_superuser:
+      return query
+    return query.filter(deleted_by=None)
+
+  def save_model(self, request, obj, form, change):
+
+    # When creating a new object, set the created_by field.
+    if not change:
+        obj.created_by = request.user
+    obj.save()
+
+  def delete_model(self, request, obj, form, change):
+
+    # When deleting an object, set the deleted_by field.
+    if not change:
+        obj.deleted_by = request.user
+    obj.delete()
--- phonebook/admin.py
+++ phonebook/admin.py
@@ -2,55 +2,19 @@ from django.contrib import admin
 
 from .models import Group, PhoneBookContact
 
+from base.admin import PhoneBookBaseAdmin
+
 # Register your models here.
 
 models_to_register = [
     Group, PhoneBookContact
 ]
 
-class GroupAdmin(admin.ModelAdmin):
+class GroupAdmin(PhoneBookBaseAdmin):
   list_display = ('id', 'name', 'description', 'created_by')
 
-  def save_model(self, request, obj, form, change):
-    """When creating a new object, set the creator field.
-    """
-    if not change:
-        obj.created_by = request.user
-    obj.save()
-
-  def delete_model(self, request, obj, form, change):
-    """When creating a new object, set the creator field.
-    """
-    if not change:
-        obj.deleted_by = request.user
-    obj.delete()
-
-class PhoneBookContactAdmin(admin.ModelAdmin):
+class PhoneBookContactAdmin(PhoneBookBaseAdmin):
   list_display = ('id', 'name', 'phone_number', 'created_by')
-
-  def get_queryset(self, request):
-    query = super(PhoneBookContactAdmin, self).get_queryset(request)
-    if request.user.is_superuser:
-      return query
-    return query.filter(deleted_by=None)
-
-  def save_model(self, request, obj, form, change):
-    """When creating a new object, set the creator field.
-    """
-    if not change:
-        obj.created_by = request.user
-    obj.save()
-
-  # def delete_model(self, request, obj, form, change):
-  #   """When creating a new object, set the creator field.
-  #   """
-  #   if not change:
-  #       obj.deleted_by = request.user
-  #   obj.delete()
-
-  def delete_model(self, request, obj):
-    obj.deleted_by = request.user
-    super(PhoneBookContactAdmin, self).delete_model(request, obj)
 
 
 def register_models(models):
