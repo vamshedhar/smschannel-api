08cdfb363634cf83cc10c5b99eade7b1c8fd390a
Vamshedhar Reddy <vamshedhar@gmail.com>
2015-02-06 00:47:10 +0530

    refactor(): minor change.

--- users/admin.py
+++ users/admin.py
@@ -69,7 +69,7 @@ class CustomUserAdmin(UserAdmin):
 		),
 	)
 
-	list_display = ('phone_number', 'first_name', 'last_name', 'email', 'is_active', 'is_staff')
+	list_display = ('id', 'phone_number', 'first_name', 'last_name', 'email', 'is_active', 'is_staff')
 	list_filter = ('is_staff', 'is_active')
 	search_fields = ('phone_number',)
 	ordering = ('first_name',)
