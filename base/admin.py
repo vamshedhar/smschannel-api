4f6c9afd0f36e915f097d0690fcfbdfef7d5a2ea
Vamshedhar Reddy <vamshedhar@gmail.com>
2015-02-05 12:41:40 +0530

    feat(smslogs): smslogs admin modified.

--- base/admin.py
+++ base/admin.py
@@ -19,12 +19,32 @@ class PhoneBookBaseAdmin(admin.ModelAdmin):
 
     # When creating a new object, set the created_by field.
     if not change:
-        obj.created_by = request.user
+      obj.created_by = request.user
     obj.save()
 
   def delete_model(self, request, obj, form, change):
 
     # When deleting an object, set the deleted_by field.
     if not change:
-        obj.deleted_by = request.user
+      obj.deleted_by = request.user
     obj.delete()
+
+class SMSBaseAdmin(admin.ModelAdmin):
+
+  class Meta:
+    abstract = True
+
+  def get_queryset(self, request):
+
+    # Override queryset to show only messages which are sent by user.
+    query = super(SMSBaseAdmin, self).get_queryset(request)
+    if request.user.is_superuser:
+      return query
+    return query.filter(sent_by=request.user)
+
+  def save_model(self, request, obj, form, change):
+
+    # When sending a new message, set the sent_by field.
+    if not change:
+      obj.sent_by = request.user
+    obj.save()
--- smslogs/admin.py
+++ smslogs/admin.py
@@ -2,15 +2,24 @@ from django.contrib import admin
 
 from .models import GroupMessage, SingleMessage
 
+from base.admin import SMSBaseAdmin
+
 # Register your models here.
 
+class GroupMessageAdmin(SMSBaseAdmin):
+  list_display = ('id', 'sent_to', 'message', 'sent_by', 'sent_on', 'delivery_status', 'created', 'modified')
+
+class SingleMessageAdmin(SMSBaseAdmin):
+  list_display = ('id', 'sent_to', 'message', 'sent_by', 'sent_on', 'delivery_status', 'created', 'modified')
+
 models_to_register = [
-    GroupMessage, SingleMessage
+  {'model_name': GroupMessage, 'model_admin': GroupMessageAdmin},
+  {'model_name': SingleMessage, 'model_admin': SingleMessageAdmin},
 ]
 
 
 def register_models(models):
-    for model in models:
-        admin.site.register(model)
+  for model in models:
+    admin.site.register(model['model_name'], model['model_admin'])
 
 register_models(models_to_register)
