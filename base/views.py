531b6b0a8c0f25be72ed69d344b4cfa39f49262b
Vamshedhar Reddy <vamshedhar@gmail.com>
2015-02-05 17:18:04 +0530

    feat(smslogs): wrote base viewset and viewsets for sms logs.

--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets, permissions
+
 # Create your views here.
+
+class SMSBaseViewset(viewsets.ModelViewSet):
+  permission_classes = (permissions.IsAuthenticated,)
+  lookup_field = 'id'
+
+  def pre_save(self, message):
+    message.sent_by = self.request.user
--- smslogs/views.py
+++ smslogs/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets, permissions
+
+from .models import GroupMessage, SingleMessage
+from .serializers import GroupMessageSerializer, SingleMessageSerializer
+
+from base.views import SMSBaseViewset
+
 # Create your views here.
+
+class GroupMessageViewset(SMSBaseViewset):
+
+  queryset = GroupMessage.objects.all()
+  serializer_class = GroupMessageSerializer
+  permission_classes = (permissions.IsAuthenticated,)
+
+class SingleMessageViewset(SMSBaseViewset):
+
+  queryset = SingleMessage.objects.all()
+  serializer_class = SingleMessageSerializer
+  permission_classes = (permissions.IsAuthenticated,)
