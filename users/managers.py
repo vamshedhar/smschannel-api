0fb1dc03985921d684242044b6523a6199584d87
Vamshedhar Reddy <vamshedhar@gmail.com>
2015-02-05 12:39:36 +0530

    refactor(): minor code refactor in users app.

--- users/managers.py
+++ users/managers.py
@@ -1,19 +1,19 @@
 from django.contrib.auth.models import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, phone_number, password=None, **extra_fields):
-        '''
-        Creates and saves a User with the given phone_number and password.
-        '''
-        user = self.model(phone_number=phone_number, is_staff=False, is_active=True, is_superuser=False, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+  def create_user(self, phone_number, password=None, **extra_fields):
+    '''
+    Creates and saves a User with the given phone_number and password.
+    '''
+    user = self.model(phone_number=phone_number, is_staff=False, is_active=True, is_superuser=False, **extra_fields)
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
 
-    def create_superuser(self, phone_number, password, **extra_fields):
-        user = self.create_user(phone_number, password, **extra_fields)
-        user.is_staff = True
-        user.is_active = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
+  def create_superuser(self, phone_number, password, **extra_fields):
+    user = self.create_user(phone_number, password, **extra_fields)
+    user.is_staff = True
+    user.is_active = True
+    user.is_superuser = True
+    user.save(using=self._db)
+    return user
