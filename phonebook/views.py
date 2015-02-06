12c94feef0d72bd81bfbf571c69808227e9bd43c
Vamshedhar Reddy <vamshedhar@gmail.com>
2015-02-06 17:16:13 +0530

    feat(api-root): api-root view added.

--- phonebook/views.py
+++ phonebook/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.reverse import reverse, reverse_lazy
 
 from .models import Group, PhoneBookContact
 from .serializers import GroupWithMembersSerializer, PhoneBookWithGroupsSerializer
@@ -8,6 +12,26 @@ from .serializers import GroupWithMembersSerializer, PhoneBookWithGroupsSerializ
 from base.views import PhoneBookBaseViewset
 
 # Create your views here.
+
+class APIRootView(APIView):
+  """
+  API Root:
+
+  List of API end point of the project.
+
+  Each list has a details view by passing ID at the end of the url.
+  """
+  def get(self, request):
+    data = {
+      'admin': reverse('admin:index', request=request),
+      'api_root': reverse('api_root', request=request),
+      'phonebook': reverse('api:phonebook-list', request=request),
+      'groups': reverse('api:groups-list', request=request),
+      'single-message': reverse('api:single-message-list', request=request),
+      'group-message': reverse('api:group-message-list', request=request),
+      'users': reverse('api:users-list', request=request),
+    }
+    return Response(data)
 
 class GroupViewset(PhoneBookBaseViewset):
 
--- smsChannelAPI/urls.py
+++ smsChannelAPI/urls.py
@@ -6,26 +6,24 @@ admin.autodiscover()
 
 from rest_framework import routers
 
+from phonebook import views as PhoneBookViews
 from smslogs.views import GroupMessageViewset, SingleMessageViewset
-from phonebook.views import GroupViewset, PhoneBookViewset
+from phonebook.views import GroupViewset, PhoneBookViewset, APIRootView
 from users.views import UserViewset
 
 router = routers.SimpleRouter()
-router.register(r'phonebook', PhoneBookViewset)
-router.register(r'groups', GroupViewset)
-router.register(r'single-message', SingleMessageViewset)
-router.register(r'group-message', GroupMessageViewset)
-router.register(r'users', UserViewset)
+router.register(r'phonebook', PhoneBookViewset, base_name="phonebook")
+router.register(r'groups', GroupViewset, base_name="groups")
+router.register(r'single-message', SingleMessageViewset, base_name="single-message")
+router.register(r'group-message', GroupMessageViewset, base_name="group-message")
+router.register(r'users', UserViewset, base_name="users")
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'smsChannelAPI.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', RedirectView.as_view(url='api/login/')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/', include(router.urls, namespace='api')),
-    url(r'^accounts/profile/$', RedirectView.as_view(url='/api/phonebook/')),
-    url(r'^auth-token/', 'rest_framework_jwt.views.obtain_jwt_token'),
+    url(r'^api/', include('rest_framework.urls', namespace="rest_framework")),
+    url(r'^api/', include(router.urls, namespace="api")),
+    url(r'^api-root/', APIRootView.as_view(), name="api_root"),
+    url(r'^accounts/profile/$', RedirectView.as_view(url='/api-root/')),
+    url(r'^auth-token/', 'rest_framework_jwt.views.obtain_jwt_token', name="auth"),
 )
