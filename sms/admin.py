bfffc42975ad36649cf7b2f5decf8d95e1defc51
Vamshedhar Reddy <vamshedhar@gmail.com>
2015-02-12 00:03:58 +0530

    feat(messageDetails): model registered with admin.

--- sms/admin.py
+++ sms/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import MessageDetails
 
+from base.admin import SMSBaseAdmin
+
 # Register your models here.
 
-admin.site.register(MessageDetails)
+class MessageDetailsAdmin(SMSBaseAdmin):
+  list_display = ('id', 'type', 'number_list', 'message', 'provider', 'message_ids', 'sent', 'status_list', 'response_code', 'sent_on')
+
+admin.site.register(MessageDetails, MessageDetailsAdmin)
